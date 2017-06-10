b8017dfed1f97de24b0f9e2d93bd3eebe93af6d3
Brian Jaress <bjaress@gmail.com>
2017-06-09 18:12:31 -0700

    haskell -- bob -- cleanup

--- haskell/bob/src/Bob.hs
+++ haskell/bob/src/Bob.hs
@@ -6,17 +6,12 @@ import qualified Data.Char as Char
 
 responseFor :: String -> String
 responseFor xs = Maybe.fromMaybe "Whatever." $
-    asSilence trimmed <|> asShout trimmed <|> asQuestion trimmed
+    asSilence nub <|> asShout nub <|> asQuestion nub
     where
-    trimmed = filter (not . Char.isSpace) xs
+    nub = filter (not . Char.isSpace) xs
 
 asSilence [] = Just "Fine. Be that way!"
 asSilence _ = Nothing
-
-asQuestion = detect . reverse
-    where
-    detect ('?':xs) = Just "Sure."
-    detect _ = Nothing
 
 asShout chars =
     if someUpper chars && noLower chars
@@ -25,3 +20,8 @@ asShout chars =
     where
     someUpper = any Char.isUpper
     noLower = all (not . Char.isLower)
+
+asQuestion = detect . reverse
+    where
+    detect ('?':xs) = Just "Sure."
+    detect _ = Nothing
