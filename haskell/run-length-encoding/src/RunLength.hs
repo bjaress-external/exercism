ccec840a0804ea1e5412552e49a1abb766c77592
Jaress, Brian (ETW - FLEX Resource) <Brian.Jaress@nike.com>
2017-07-10 12:16:00 -0700

    haskell -- run-length-encoding -- solution 1
    parsec for decoding
    string splitting for encoding

--- haskell/run-length-encoding/src/RunLength.hs
+++ haskell/run-length-encoding/src/RunLength.hs
@@ -1,7 +1,37 @@
 module RunLength (decode, encode) where
 
+import qualified Data.List as List
+import qualified Text.Parsec as Parsec
+import qualified Text.Parsec.Char as PChar
+import Control.Monad ((>=>))
+import Control.Applicative ((<|>), (<$>))
+
+type CharRun = (Int, Char)
+
 decode :: String -> String
-decode encodedText = error "You need to implement this function."
+decode = runs >=> dec
+    where
+    runs :: String -> [CharRun]
+    runs = either (const []) id . Parsec.parse parser ""
+
+    parser :: Parsec.Parsec String st [CharRun]
+    parser = Parsec.many $ numberedRun <|> singleton
+    -- "123a" to (123, 'a')
+    numberedRun = (,) . read <$> Parsec.many1 PChar.digit <*> PChar.anyChar
+    -- "a" to (1, 'a')
+    singleton = (,) 1 <$> PChar.anyChar
+
+    -- (3, 'a') to "aaa"
+    dec = uncurry replicate
+
 
 encode :: String -> String
-encode text = error "You need to implement this function."
+encode = runs >=> enc
+    where
+    runs :: String -> [CharRun]
+    runs = fmap extract . List.group
+    extract cs = (length cs, head cs)
+
+    enc :: CharRun -> String
+    enc (1, c) = [c]
+    enc (n, c) = show n ++ [c]
