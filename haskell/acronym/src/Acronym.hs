60851e1fbd561cc346b540031f7cd1f24d8287c3
Brian Jaress <bjaress@gmail.com>
2017-09-17 16:06:28 -0700

    haskell -- acronyms -- ugly but working

--- haskell/acronym/src/Acronym.hs
+++ haskell/acronym/src/Acronym.hs
@@ -1,4 +1,22 @@
 module Acronym (abbreviate) where
 
+import qualified Text.Parsec as Parsec
+import qualified Text.Parsec.Char as PChar
+import Control.Applicative ((<|>), (<$>))
+import Data.Char as Char
+
+
 abbreviate :: String -> String
-abbreviate xs = error "You need to implement this function."
+abbreviate = either (const []) id . Parsec.parse parser ""
+    where
+    parser :: Parsec.Parsec String st String
+    parser = initials <$> Parsec.sepBy word noise
+
+    noise = Parsec.many $ PChar.satisfy (not . Char.isAlpha)
+
+    word = allLower <|> someHigher
+    allLower = Parsec.many1 PChar.lower
+    someHigher = concat <$> sequence [ Parsec.many1 PChar.upper, Parsec.many PChar.lower]
+
+initials :: [String] -> String
+initials = fmap (Char.toUpper . head)
