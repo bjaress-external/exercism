527cdbd258f3864822bee43e1d7567e4d99eca10
Brian Jaress <bjaress@gmail.com>
2017-10-15 21:17:31 -0700

    haskell -- nucleotide counts -- attempted cleanup

--- haskell/nucleotide-count/src/DNA.hs
+++ haskell/nucleotide-count/src/DNA.hs
@@ -17,9 +17,12 @@ count = Map.fromListWith (+) . fmap (increment . flagBad)
         | otherwise = bad
 
 validate totals
-    | Map.member bad totals = Left (show $ Map.lookup bad totals)
+    | Map.member bad totals = Left $ countBad totals
     | otherwise = (Right . Map.union totals . zeros) nucleotides
     where
+    countBad = show . Map.lookup bad
+    normalize :: Map.Map Char Int -> Map.Map Char Int
+    normalize = flip Map.union $ zeros nucleotides
     zeros = Map.fromList . fmap (pairWith 0)
 
 pairWith = flip (,)
