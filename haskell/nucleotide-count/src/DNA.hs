cf4ea46304dd61ca2850239953a9a9e7f6d06228
Brian Jaress <bjaress@gmail.com>
2017-10-15 18:26:31 -0700

    haskell -- nucleotide count -- working but ugly

--- haskell/nucleotide-count/src/DNA.hs
+++ haskell/nucleotide-count/src/DNA.hs
@@ -3,6 +3,7 @@ module DNA (nucleotideCounts) where
 import qualified Data.Map as Map
 
 nucleotides = "ACGT"
+unknown = 'X'
 
 zeros = Map.fromList [(nuc, 0) | nuc <- nucleotides]
 
@@ -11,9 +12,9 @@ nucleotideCounts = validate . count
 
 count::String -> Map.Map Char Int
 count xs = Map.fromListWith (+) pairs
-    where pairs = [(if elem x "ACGT" then x else 'X', 1) | x <- xs]
+    where pairs = [(if elem x nucleotides then x else unknown, 1) | x <- xs]
 
 
 validate map
-    | Map.member 'X' map = Left "non-nucleotide"
+    | Map.member unknown map = Left "non-nucleotide"
     | otherwise = Right $ Map.union map zeros
