aa46a9908afc20d5d6be084db5ff37bad9aa5f67
Jaress, Brian (ETW - FLEX Resource) <Brian.Jaress@nike.com>
2017-10-18 15:56:04 -0700

    haskell -- grade school -- start of solution

--- haskell/grade-school/src/School.hs
+++ haskell/grade-school/src/School.hs
@@ -1,15 +1,18 @@
 module School (School, add, empty, grade, sorted) where
 
-data School = Dummy
+import qualified Data.IntMap.Lazy as Map
+import qualified Data.Set as Set
+
+type School = Map.IntMap (Set.Set String)
 
 add :: Int -> String -> School -> School
 add gradeNum student school = error "You need to implement this function."
 
 empty :: School
-empty = error "You need to implement this function."
+empty = Map.empty
 
 grade :: Int -> School -> [String]
 grade gradeNum school = error "You need to implement this function."
 
 sorted :: School -> [(Int, [String])]
-sorted school = error "You need to implement this function."
+sorted = (fmap . fmap) Set.toAscList . Map.toAscList
