ca46e33b340db593f28577ba7988d6b11a3fe0b3
Jaress, Brian (ETW - FLEX Resource) <Brian.Jaress@nike.com>
2017-10-18 17:04:13 -0700

    haskell -- grade-school -- messy version of grade

--- haskell/grade-school/src/School.hs
+++ haskell/grade-school/src/School.hs
@@ -2,6 +2,7 @@ module School (School, add, empty, grade, sorted) where
 
 import qualified Data.IntMap.Lazy as Map
 import qualified Data.Set as Set
+import qualified Data.Maybe as Maybe
 
 type School = Map.IntMap (Set.Set String)
 
@@ -12,7 +13,7 @@ empty :: School
 empty = Map.empty
 
 grade :: Int -> School -> [String]
-grade gradeNum school = concat $ fmap Set.toAscList $  maybeToList $ Map.lookup grade school
+grade gradeNum school = concat $ fmap Set.toAscList $  Maybe.maybeToList $ Map.lookup gradeNum school
 
 sorted :: School -> [(Int, [String])]
 sorted = (fmap . fmap) Set.toAscList . Map.toAscList
