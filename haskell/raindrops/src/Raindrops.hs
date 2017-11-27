d2dc80e89a4c2ce85f030bbcd854a2af6fc56632
Brian Jaress <bjaress@gmail.com>
2017-11-26 18:47:14 -0800

    haskell -- raindrops -- combining with multiplication rather than logical and

--- haskell/raindrops/src/Raindrops.hs
+++ haskell/raindrops/src/Raindrops.hs
@@ -2,17 +2,20 @@ module Raindrops (convert) where
 
 convert :: Int -> String
 convert n
-    | pling n && plang n && plong n = "PlingPlangPlong"
-    | pling n && plang n = "PlingPlang"
-    | pling n && plong n = "PlingPlong"
-    | plang n && plong n = "PlangPlong"
-    | pling n = "Pling"
-    | plang n = "Plang"
-    | plong n = "Plong"
+    | check n $ pling * plang * plong = "PlingPlangPlong"
+
+    | check n $ pling * plang = "PlingPlang"
+    | check n $ pling * plong = "PlingPlong"
+    | check n $ plang * plong = "PlangPlong"
+
+    | check n pling = "Pling"
+    | check n plang = "Plang"
+    | check n plong = "Plong"
+
     | otherwise = show n
 
-pling = check 3
-plang = check 5
-plong = check 7
-
-check k n = n `mod` k == 0
+    where
+    pling = 3
+    plang = 5
+    plong = 7
+    check n k = n `mod` k == 0
