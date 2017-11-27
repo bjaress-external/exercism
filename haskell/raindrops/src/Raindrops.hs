310b84f0c7c10f3e77ee97c1074f91e88ec75f4b
Brian Jaress <bjaress@gmail.com>
2017-11-26 18:23:21 -0800

    haskell -- raindrops -- working verbosely

--- haskell/raindrops/src/Raindrops.hs
+++ haskell/raindrops/src/Raindrops.hs
@@ -1,4 +1,16 @@
 module Raindrops (convert) where
 
 convert :: Int -> String
-convert n = error "You need to implement this function."
+convert n
+    | pling n && plang n && plong n = "PlingPlangPlong"
+    | pling n && plang n = "PlingPlang"
+    | pling n && plong n = "PlingPlong"
+    | plang n && plong n = "PlangPlong"
+    | pling n = "Pling"
+    | plang n = "Plang"
+    | plong n = "Plong"
+    | otherwise = show n
+
+pling n = n `mod` 3 == 0
+plang n = n `mod` 5 == 0
+plong n = n `mod` 7 == 0
