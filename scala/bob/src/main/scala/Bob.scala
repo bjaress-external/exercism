ed8e72f914b4396e22b359b10bbe0908f60dadc0
Brian Jaress <bjaress@gmail.com>
2018-03-08 14:10:16 -0800

    scala -- bob -- complete

--- scala/bob/src/main/scala/Bob.scala
+++ scala/bob/src/main/scala/Bob.scala
@@ -1,3 +1,22 @@
 object Bob {
-  def response(statement: String): String = ???
+
+  def response(statement: String): String =
+    if (isShouting(statement) && isQuestion(statement))
+      "Calm down, I know what I'm doing!"
+    else if (isShouting(statement))
+      "Whoa, chill out!"
+    else if (isQuestion(statement))
+      "Sure."
+    else if (isEmpty(statement))
+      "Fine. Be that way!"
+    else
+      "Whatever."
+
+  def isQuestion(statement: String): Boolean =
+    statement.reverse.dropWhile(_.isWhitespace).startsWith("?")
+
+  def isShouting(statement: String): Boolean =
+    !statement.exists(_.isLower) && statement.exists(_.isUpper)
+
+  def isEmpty(statement: String): Boolean = statement.forall(_.isWhitespace)
 }
