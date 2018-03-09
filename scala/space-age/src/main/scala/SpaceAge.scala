4308747715cb376c64972066f1feccaedae6f86c
Brian Jaress <bjaress@gmail.com>
2018-03-09 07:26:51 -0800

    scala -- space-age -- curried

--- scala/space-age/src/main/scala/SpaceAge.scala
+++ scala/space-age/src/main/scala/SpaceAge.scala
@@ -1,10 +1,20 @@
 object SpaceAge {
-  def onEarth(seconds: Double) = 31.69
-  def onMercury(seconds: Double) = 280.88
-  def onVenus(seconds: Double) = 9.78
-  def onMars(seconds: Double) = 39.25
-  def onJupiter(seconds: Double) = 2.41
-  def onSaturn(seconds: Double) = 3.23
-  def onUranus(seconds: Double) = 1.21
-  def onNeptune(seconds: Double) = 1.58
+  val secondsPerEarthYear = 31557600
+
+  def planetYears(earthYearsPerPlanetYear: Double)(seconds: Double): Double =
+    round(seconds / secondsPerEarthYear / earthYearsPerPlanetYear)
+
+  def round(raw: Double): Double =
+    (raw * 100).round / 100.toDouble
+
+  // I find this syntax wacky, but the compiler literally told me to.
+  val onEarth = planetYears(1) _
+  val onMercury = planetYears(0.2408467) _
+  val onVenus = planetYears(0.61519726) _
+  val onMars = planetYears(1.8808158) _
+  val onJupiter = planetYears(11.862615) _
+  val onSaturn = planetYears(29.447498) _
+  val onUranus = planetYears(84.016846) _
+  val onNeptune = planetYears(164.79132) _
+
 }
