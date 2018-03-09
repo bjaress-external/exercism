70150d7f3f0c8f98aebb6492f822c42743102b46
Brian Jaress <bjaress@gmail.com>
2018-03-09 06:39:34 -0800

    scala -- space-age -- hard-coded

---
+++ scala/space-age/src/main/scala/SpaceAge.scala
@@ -0,0 +1,10 @@
+object SpaceAge {
+  def onEarth(seconds: Double) = 31.69
+  def onMercury(seconds: Double) = 280.88
+  def onVenus(seconds: Double) = 9.78
+  def onMars(seconds: Double) = 39.25
+  def onJupiter(seconds: Double) = 2.41
+  def onSaturn(seconds: Double) = 3.23
+  def onUranus(seconds: Double) = 1.21
+  def onNeptune(seconds: Double) = 1.58
+}
