8ebe677303e4472350c662ea5893a17ad85bcaf0
Brian Jaress <Brian.Jaress@healthsparq.com>
2019-08-09 17:08:44 -0700

    clojure-external solved bob

--- clojure/bob/src/bob.clj
+++ clojure/bob/src/bob.clj
@@ -1,5 +1,24 @@
 (ns bob)
 
-(defn response-for [s] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn lower? [s]
+  (re-find #"[a-z]" s))
+
+(defn upper? [s]
+  (re-find #"[A-Z]" s))
+
+(defn shout? [s]
+  (and (upper? s) (not (lower? s))))
+
+(defn question? [s]
+  (re-matches #".*\?$" s))
+
+(defn nothing? [s]
+  (re-matches #"\W*" s))
+
+(defn response-for [s]
+  (cond
+    (and (shout? s) (question? s)) "Calm down, I know what I'm doing!"
+    (shout? s) "Whoa, chill out!"
+    (question? s) "Sure."
+    (nothing? s) "Fine. Be that way!"
+    :else "Whatever."))
