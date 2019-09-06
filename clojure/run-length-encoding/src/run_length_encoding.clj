a35a9f85073c16b7e4bf35c8cf867c46bf9c3766
Brian Jaress <Brian.Jaress@healthsparq.com>
2019-09-06 15:19:32 -0700

    clojure-external solved run-length-encoding

--- clojure/run-length-encoding/src/run_length_encoding.clj
+++ clojure/run-length-encoding/src/run_length_encoding.clj
@@ -2,8 +2,20 @@
 
 (defn run-length-encode
   "encodes a string with run-length-encoding"
-  [plain-text])
+  [plain-text]
+  (with-out-str
+    (doseq [chunk (partition-by identity plain-text)]
+      (if (> (count chunk) 1)
+        (print (count chunk)))
+      (print (first chunk)))))
 
 (defn run-length-decode
   "decodes a run-length-encoded string"
-  [cipher-text])
+  [cipher-text]
+  (with-out-str
+    (doseq [[_ prefix letter] (re-seq #"([0-9]*)([^0-9])" cipher-text)]
+      (def repeats
+        (if (= "" prefix)
+          1
+          (bigint prefix)))
+      (dotimes [_ repeats] (print letter)))))
