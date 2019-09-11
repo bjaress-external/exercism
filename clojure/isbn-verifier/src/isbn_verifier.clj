1c68e143d49640b20c3e9abbd445ffa5ce335602
Brian Jaress <Brian.Jaress@healthsparq.com>
2019-09-11 15:24:37 -0700

    clojure-external isbn-verifier solved

--- clojure/isbn-verifier/src/isbn_verifier.clj
+++ clojure/isbn-verifier/src/isbn_verifier.clj
@@ -1,5 +1,13 @@
 (ns isbn-verifier)
 
-(defn isbn? [isbn] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn isbn-sum [digits]
+  (defn value-at [index digit]
+    (def value (if (= "X" digit) 10 (bigint digit)))
+    (* (- 10 index) value))
+  (mod (apply + (keep-indexed value-at digits)) 11))
+
+(defn isbn? [isbn]
+  (if (nil? (re-find #"^([0-9]-*){9}[0-9X]$" isbn))
+    false
+    (= 0 (isbn-sum (map (fn [[_ digit]] digit)
+                        (re-seq #"([0-9X])-*" isbn))))))
