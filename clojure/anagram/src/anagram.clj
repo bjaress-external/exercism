306e46a5065d94cd608431914e2b6cdbb4bb60eb
Brian Jaress <Brian.Jaress@healthsparq.com>
2019-09-27 16:27:52 -0700

    clojure-external solved anagram

--- clojure/anagram/src/anagram.clj
+++ clojure/anagram/src/anagram.clj
@@ -1,5 +1,15 @@
-(ns anagram)
+(ns anagram
+  (:require [clojure.string :as str]))
 
-(defn anagrams-for [word prospect-list] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn letter-bag [word]
+  (apply merge-with +
+         (map (fn [letter] {letter 1}) word)))
+
+(defn match [target prospect]
+  (let [t (str/lower-case target)
+        p (str/lower-case prospect)]
+    (and (= (letter-bag t) (letter-bag p))
+         (not (= t p)))))
+
+(defn anagrams-for [word prospect-list]
+  (filter (partial match word) prospect-list))
