1dff6111678ab71073753a919751fa2470f19e55
Brian Jaress <Brian.Jaress@healthsparq.com>
2019-09-30 15:29:00 -0700

    clojure-external wip using frequencies

--- clojure/anagram/src/anagram.clj
+++ clojure/anagram/src/anagram.clj
@@ -1,14 +1,10 @@
 (ns anagram
   (:require [clojure.string :as str]))
 
-(defn letter-bag [word]
-  (apply merge-with +
-         (map (fn [letter] {letter 1}) word)))
-
 (defn match [target prospect]
   (let [t (str/lower-case target)
         p (str/lower-case prospect)]
-    (and (= (letter-bag t) (letter-bag p))
+    (and (= (frequencies t) (frequencies p))
          (not (= t p)))))
 
 (defn anagrams-for [word prospect-list]
