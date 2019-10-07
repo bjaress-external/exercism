b77df7190f4d607ba1b84a597863471841605ca4
Brian Jaress <Brian.Jaress@healthsparq.com>
2019-10-07 16:29:08 -0700

    clojure-external solved say

--- clojure/say/project.clj
+++ clojure/say/project.clj
@@ -1,4 +1,5 @@
 (defproject say "0.1.0-SNAPSHOT"
   :description "say exercise."
   :url "https://github.com/exercism/clojure/tree/master/exercises/say"
-  :dependencies [[org.clojure/clojure "1.10.0"]])
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/math.numeric-tower "0.0.4"]])
--- clojure/say/src/say.clj
+++ clojure/say/src/say.clj
@@ -1,5 +1,61 @@
-(ns say)
+(ns say
+  (:require
+    [clojure.string :as str]
+    [clojure.math.numeric-tower :as numeric]))
 
-(defn number [num] ;; <- arglist goes here
-  ;; your code goes here
-)
+(def very-small
+  [ "zero"
+    "one"
+    "two"
+    "three"
+    "four"
+    "five"
+    "six"
+    "seven"
+    "eight"
+    "nine"
+    "ten"
+    "eleven"
+    "twelve"
+    "thirteen"
+    "fourteen"
+    "fifteen"
+    "sixteen"
+    "seventeen"
+    "eighteen"
+    "nineteen"
+    ])
+
+(def tens
+  [ "zero"
+    "ten"
+    "twenty"
+    "thirty"
+    "forty"
+    "fifty"
+    "sixty"
+    "seventy"
+    "eighty"
+    "ninety"
+    ])
+
+(defn handle-parts [num divisor handle-upper separator handle-lower]
+  (let [upper (quot num divisor)
+        lower (rem num divisor)]
+    [(handle-upper upper) separator (handle-lower lower)]))
+
+(defn zeros [count] (numeric/expt 10 count))
+
+(defn say [num]
+  (cond
+    (< num 0) (throw (IllegalArgumentException. "Too small!"))
+    (< num (count very-small)) (very-small num)
+    (< num 100) (str/join (handle-parts num 10 tens "-" very-small))
+    (< num (zeros 3)) (str/join " " (handle-parts num (zeros 2) say "hundred" say))
+    (< num (zeros 6)) (str/join " " (handle-parts num (zeros 3) say "thousand" say))
+    (< num (zeros 9)) (str/join " " (handle-parts num (zeros 6) say "million" say))
+    (< num (zeros 12)) (str/join " " (handle-parts num (zeros 9) say "billion" say))
+    :else (throw (IllegalArgumentException. "Too large!"))))
+
+(defn number [num]
+  (str/replace (say num) #".zero$" ""))
